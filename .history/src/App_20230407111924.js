889f129736f2c9ee595412ccf0c9fae6a9d69dbd
AlexanderBil <deos.ninja@gmail.com>
2023-04-07 11:23:49 +0300

    4

--- .history/src/App_20230407111924.js
+++ .history/src/App_20230407111924.js
@@ -3,10 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>1</p>
-      <p>2</p>
      <p>2</p>
-     <p>3</p>
     </div>
   );
 }
--- .history/src/App_20230407111927.js
+++ .history/src/App_20230407111927.js
@@ -3,8 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>1</p>
-      <p>2</p>
      <p>2</p>
      <p>3</p>
     </div>
